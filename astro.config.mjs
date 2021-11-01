d3f2e964fa8b7375ceda68ae7cec65e71aec992c
Thomas Stachl <thomas@stachl.me>
2021-10-31 19:11:36 -0700

    some more changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,8 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the Preact renderer to support Preact JSX components.
   renderers: ['@astrojs/renderer-preact'],
   devOptions: {
-    tailwindConfig: './tailwind.config.js'
+    tailwindConfig: './tailwind.config.js',
+    trailingSlash: 'ignore',
   },
   buildOptions: {
     site: "https://stachl.me"
