c1116777a7a63866b9de663a83b0e61d7ae61712
Thomas Stachl <thomas@stachl.me>
2021-11-15 10:27:40 -0800

    youtube thumbnail downloader

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
+import astroRemark from "@astrojs/markdown-remark";
+
 const { CF_PAGES_BRANCH, DOMAIN } = process.env;
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the Preact renderer to support Preact JSX components.
   renderers: ["@astrojs/renderer-preact"],
+  markdownOptions: {
+    render: [
+      astroRemark,
+      {
+        remarkPlugins: [], // ["remark-code-titles", "remark-slug"],
+        rehypePlugins: [
+          // ["rehype-autolink-headings", { behavior: "prepend" }],
+          // ["rehype-toc", { headings: ["h2", "h3"] }],
+          // ["rehype-add-classes", { "h1,h2,h3": "title" }],
+          ["../../../../plugins/youtube-thumbnail.mjs"],
+        ],
+      },
+    ],
+  },
   devOptions: {
     tailwindConfig: "./tailwind.config.js",
     trailingSlash: "ignore",
